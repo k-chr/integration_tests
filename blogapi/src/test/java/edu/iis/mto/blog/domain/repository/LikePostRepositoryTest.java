1c5ef6877dc2f86f9d899fb4498263c3ac8041de
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-21 23:37:23 +0200

    Prepared class for LikePostRepository tests

---
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -0,0 +1,30 @@
+package edu.iis.mto.blog.domain.repository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class LikePostRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+    
+    @Autowired
+    private LikePostRepository repository;
+
+    @Before
+    public void setUp() {
+        
+    }
+
+    @Test
+    public void templateTest() {
+        
+    }
+}
