3c9ac20766351f56c0af4c8835820081d2c5983a
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-22 23:49:14 +0200

    reorganized code structure, added test case for adding like to post by confirmed user

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,15 +1,19 @@
 package edu.iis.mto.blog.domain;
 
+import static edu.iis.mto.blog.domain.repository.LikePostMatcher.hasContentSuchAs;
+import static edu.iis.mto.blog.domain.repository.LikePostMatcher.isLikedBy;
+
 import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 import edu.iis.mto.blog.domain.errors.DomainError;
 import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
 import edu.iis.mto.blog.domain.repository.BlogPostRepository;
 import edu.iis.mto.blog.domain.repository.LikePostRepository;
+
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -59,20 +63,20 @@ public class BlogManagerTest {
     private User theManWhoLikes;
     private BlogPost post;
 
-    @Test
-    public void creatingNewUserShouldSetAccountStatusToNEW() {
-        blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
-        verify(userRepository).save(userParam.capture());
-        User user = userParam.getValue();
-        assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
-    }
-
     @Before
     public void setUp()  {
         initEntities();
         initUser(owner, "Edward", "McDonald", "some_mail@example.com", AccountStatus.NEW, FAKE_USER_ID_1);
         initUser(theManWhoLikes, "Sigfrid", "Lehman", "sig@elpmaxe.com", AccountStatus.NEW, FAKE_USER_ID_2);
         initBlogPost(post, owner, "Example data", FAKE_POST_ID_1);
+    }
+
+    @Test
+    public void creatingNewUserShouldSetAccountStatusToNEW() {
+        blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
+        verify(userRepository).save(userParam.capture());
+        User user = userParam.getValue();
+        assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
     }
 
     @Test
@@ -85,6 +89,22 @@ public class BlogManagerTest {
         assertThrows(DomainError.class, () -> blogService.addLikeToPost(FAKE_USER_ID_2, FAKE_POST_ID_1));
     }
 
+    @Test
+    public void ifConfirmedUserTriedToLikePostOfOtherUserRepositoryShouldSaveLike(){
+        theManWhoLikes.setAccountStatus(AccountStatus.CONFIRMED);
+        when(userRepository.findById(FAKE_USER_ID_1)).thenReturn(Optional.of(owner));
+        when(userRepository.findById(FAKE_USER_ID_2)).thenReturn(Optional.of(theManWhoLikes));
+        when(blogPostRepository.findById(FAKE_POST_ID_1)).thenReturn(Optional.of(post));
+        when(likePostRepository.findByUserAndPost(theManWhoLikes, post)).thenReturn(Optional.empty());
+
+        blogService.addLikeToPost(FAKE_USER_ID_2, FAKE_POST_ID_1);
+        var likePostCaptor = ArgumentCaptor.forClass(LikePost.class);
+        verify(likePostRepository).save(likePostCaptor.capture());
+        var likePost = likePostCaptor.getValue();
+        assertThat(likePost, isLikedBy(theManWhoLikes));
+        assertThat(likePost, hasContentSuchAs(post));
+    }
+    
     private void initEntities(){
         owner = new User();
         theManWhoLikes = new User();
