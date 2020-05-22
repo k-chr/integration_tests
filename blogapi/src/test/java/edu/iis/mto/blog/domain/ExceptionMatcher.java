f46f914e1ffbf86350b96258c585decf41187335
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-23 00:11:09 +0200

    Added exception matcher for matching messages from various DomainError type exceptions

---
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/ExceptionMatcher.java
@@ -0,0 +1,29 @@
+package edu.iis.mto.blog.domain;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class ExceptionMatcher extends TypeSafeMatcher<Exception> {
+
+    private String message;
+
+    private ExceptionMatcher(String message) {
+
+        this.message = message;
+    }
+
+    public static Matcher<Exception> hasMessage(String message){
+        return new ExceptionMatcher(message);
+    }
+
+    @Override
+    protected boolean matchesSafely(Exception e) {
+        return e.getMessage().equals(message);
+    }
+
+    @Override
+    public void describeTo(Description description) {
+
+    }
+}
