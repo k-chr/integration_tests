56e625d59a7477b45b37694ff5194ed551bd1f56
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-24 00:55:19 +0200

    Added users' status filtering before returning collection of matching users.

--- blogapi/src/main/java/edu/iis/mto/blog/domain/BlogDataFinder.java
+++ blogapi/src/main/java/edu/iis/mto/blog/domain/BlogDataFinder.java
@@ -42,6 +42,7 @@ public class BlogDataFinder extends DomainService implements DataFinder {
                 searchString, searchString);
 
         return users.stream()
+                    .filter(BlogDataFinder::isNotRemoved)
                     .map(mapper::mapToDto)
                     .collect(Collectors.toList());
     }
@@ -69,4 +70,7 @@ public class BlogDataFinder extends DomainService implements DataFinder {
                     .collect(Collectors.toList());
     }
 
+    private static boolean isNotRemoved(User user){
+        return user.getAccountStatus() != AccountStatus.REMOVED;
+    }
 }
