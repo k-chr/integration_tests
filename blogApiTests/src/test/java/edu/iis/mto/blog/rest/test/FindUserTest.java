d0826c1af711bb42794ec0a964e18f0432a452c3
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-24 00:49:15 +0200

    Added test case implementation for removed user, it fails, probably removed users are not filtered from result collection on server side

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FindUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FindUserTest.java
@@ -1,12 +1,29 @@
 package edu.iis.mto.blog.rest.test;
 
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
+
+import static edu.iis.mto.blog.rest.test.TestConstants.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 
 public class FindUserTest extends FunctionalTests {
 
     @Test
     void attemptToFindRemovedUserByLastNameShouldEndUpWithResponseSuccessWithOkStatusAndEmptyListOfUsers() {
-
+        given()
+                .accept(ContentType.JSON)
+                .header(TYPE, OPTION_JSON)
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .body("size()", is(equalTo(0)))
+                .when()
+                .get(findUserByStringApi(REMOVED_USER_SEARCH_STRING));
     }
 
     @Test
@@ -46,6 +63,6 @@ public class FindUserTest extends FunctionalTests {
 
     @Test
     void attemptToFindValidUsersByEmptyStringShouldEndUpWithResponseSuccessWithOkStatusAndResultListThatContainsAllUsersWithoutRemovedOnes() {
-        
+
     }
 }
