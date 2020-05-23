bcb6f3e4463d65ce8635e8cad0856ee416c6a3aa
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-23 16:56:22 +0200

    Added basic test cases definitions, added method testJson to generate json string for BlogPost object to create.

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddBlogPostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddBlogPostTest.java
@@ -1,4 +1,34 @@
 package edu.iis.mto.blog.rest.test;
 
+import static io.restassured.RestAssured.given;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
 public class AddBlogPostTest extends FunctionalTests {
+
+    @Test
+    void attemptToAddPostByNewUserShouldEndUpWithResponseErrorWithBadRequestStatus() {
+
+    }
+
+    @Test
+    void attemptToAddPostByConfirmedUserShouldEndUpWithResponseSuccessWithCreatedStatus() {
+
+    }
+
+    @Test
+    void attemptToAddPostByRemovedUserShouldEndUpWithResponseErrorWithBadRequestStatus() {
+
+    }
+
+    @Test
+    void attemptToAddPostByNotExistingUserShouldEndUpWithResponseErrorWithNotFoundStatus() {
+
+    }
+
+    private String testJson(){
+        return new JSONObject().put("","").toString();
+    }
 }
