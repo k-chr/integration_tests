5e3839bdb12572d92e5cf7f99ee02272ff055339
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-23 17:40:48 +0200

    extracted common constants to FunctionalTests class, added one test implementation, but it fails, it has to be inspected why API permits to add post by NEW user.

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddBlogPostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddBlogPostTest.java
@@ -2,15 +2,29 @@ package edu.iis.mto.blog.rest.test;
 
 import static io.restassured.RestAssured.given;
 
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 public class AddBlogPostTest extends FunctionalTests {
 
+    private final static int USER_NEW = 0x02;
+    private final static int USER_CONFIRMED = 0x01;
+    private final static int USER_REMOVED = 0x03;
+    private final static int USER_THAT_NOT_EXISTS = 0xFFFFFFFF;
+
     @Test
     void attemptToAddPostByNewUserShouldEndUpWithResponseErrorWithBadRequestStatus() {
-
+        given().accept(ContentType.JSON)
+               .header(TYPE, OPTION_JSON)
+               .body(testJson())
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_BAD_REQUEST)
+               .when()
+               .post(createPostApiForId(USER_NEW));
     }
 
     @Test
@@ -29,6 +43,6 @@ public class AddBlogPostTest extends FunctionalTests {
     }
 
     private String testJson(){
-        return new JSONObject().put("","").toString();
+        return new JSONObject().put("entry","some not important notes").toString();
     }
 }
