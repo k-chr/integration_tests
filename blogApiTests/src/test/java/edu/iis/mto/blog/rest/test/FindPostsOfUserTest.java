6daefbf238b519c195a1ab43a27b5f477e8c3ec6
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-23 22:43:30 +0200

    prepared definitions for test cases

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FindPostsOfUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FindPostsOfUserTest.java
@@ -1,5 +1,31 @@
 package edu.iis.mto.blog.rest.test;
 
+import org.junit.jupiter.api.Test;
+
 public class FindPostsOfUserTest extends FunctionalTests {
 
+    @Test
+    void attemptToFindPostsOfRemovedUserShouldEndUpWithResponseErrorWithBadRequestStatus() {
+
+    }
+
+    @Test
+    void attemptToFindPostsOfValidUserShouldEndUpWithResponseSuccessWithOkStatusAndLikedPostWithCorrectCountOfLikes() {
+
+    }
+
+    @Test
+    void attemptToFindPostsOfNonExistingUserShouldEndUpWithResponseErrorWithNotFoundStatus() {
+
+    }
+
+    @Test
+    void attemptToFindPostsOfValidUserWhoHasNoPostsShouldEndUpWithResponseSuccessWithOkStatusAndEmptyListOfPosts() {
+
+    }
+
+    @Test
+    void attemptToFindPostsOfValidUserWhoHasPostsShouldEndUpWithResponseSuccessWithOkStatusAndListOfPostsWithCorrectCount() {
+
+    }
 }
