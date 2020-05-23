a9086282111b4ed74728747b03d23cd554ad17e6
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-23 18:54:17 +0200

    Prepared new test data for LikePostAPI

--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -2,3 +2,6 @@
 insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'john@domain.com', 'John', 'Steward')
 insert into user (account_status, email, first_name) values ('NEW', 'brian@domain.com', 'Brian')
 insert into user (account_status, email, first_name, last_name) values ('REMOVED', 'john_cena@domain.com', 'John', 'Cena')
+insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'edward_ącki@domain.com', 'Edward', 'Ącki')
+insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'is_not_this_the_song_with_the_fibonacci_sequence_question_mark@domain.com', 'Tool', 'Lateralus')
+insert into blog_post values (null, 'some not important notes of confirmed user with id 1', 1)
