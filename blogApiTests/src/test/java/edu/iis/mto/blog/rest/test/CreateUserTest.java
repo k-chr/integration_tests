b043b685e88d5245b7e21d555269530580c8d893
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-23 15:14:55 +0200

    extracted constants

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -11,12 +11,14 @@ import io.restassured.http.ContentType;
 public class CreateUserTest extends FunctionalTests {
 
     private static final String USER_API = "/blog/user";
+    private static final String OPTION_JSON = "application/json;charset=UTF-8";
+    private static final String TYPE = "Content-Type";
 
     @Test
     public void createUserWithProperDataReturnsCreatedStatus() {
         JSONObject jsonObj = new JSONObject().put("email", "tracy1@domain.com");
         given().accept(ContentType.JSON)
-               .header("Content-Type", "application/json;charset=UTF-8")
+               .header(TYPE, OPTION_JSON)
                .body(jsonObj.toString())
                .expect()
                .log()
@@ -30,7 +32,7 @@ public class CreateUserTest extends FunctionalTests {
     public void attemptToCreateUserFromPostJsonWithNotUniqueEmailShouldReturnErrorWithHTTPConflictCode () {
         var json = new JSONObject().put("email", "brian@domain.com").toString();
         given().accept(ContentType.JSON)
-                .header("Content-Type","application/json;charset=UTF-8")
+                .header(TYPE, OPTION_JSON)
                 .body(json)
                 .expect()
                 .log()
